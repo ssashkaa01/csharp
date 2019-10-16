6746c4cc677277a80595396c51784a2b58277068
STORYM <ssashkaa01@gmail.com>
2019-10-16 20:52:05 +0300

    add hw

---
+++ 17.10.2019/17.10.2019/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._10._2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Завдання 1:
+            Дано цілочисельну послідовність.
+            Витягти з неї всі позитивні числа, 
+            відсортувавши їх по зростанню. (можна використати Where, OrderBy)
+            */
+
+            //int[] numbers = { -7, 3, 45, -67 , 12, -9, 90, 45 };
+
+            //var res = from num in numbers
+            //          where num >= 0
+            //          orderby num ascending
+            //          select num;
+            //foreach(var item in res)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            /*
+            Завдання 2:
+            Дано колекцію цілих чисел. 
+            Знайти кількість позитивних двозначних елементів, 
+            а також їх середнє арифметичне. (Where, Avg)
+            */
+
+
+            //int[] numbers = { -7, 30, 45, -67 , 12, -9, 90, 45 };
+
+            //var res = from num in numbers
+            //          where num % 2 == 0
+            //          orderby num ascending
+            //          select num;
+
+            //foreach (var item in res)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine($"Count: {res.Count()}");
+            //Console.WriteLine($"Average: {res.Average()}");
+
+            /*
+            Завдання 3:
+            Дано колекцію непустих рядків. 
+            Отримати рядок, що складається з початкових символів 
+            всіх рядків вхідної послідовності. (Aggregate)
+            */
+
+            //string[] strings = { "sdfsdg", "aadlkflk", "werwewe", "deweoiewio", "easdgsdgasdg" };
+
+            //var res = strings.Aggregate($"", (concat, str) => $"{concat}{str[0]}");
+
+            //Console.WriteLine(res);
+
+            /*
+            Завдання 4:
+            Дано колекцію непустих рядків A.
+            Отримати колекцію символів, 
+            кожен елемент якої є початковим символом відповідного рядка з A. 
+            Порядок символів повинен бути зворотнім по відношенню до порядку елементів вхідної колекції. 
+            (SelectMany з урахуванням того, що рядок можна конвертувати в колекцію символів). 
+            */
+
+            //string[] strings = { "sdfsdg", "aadlkflk", "werwewe", "deweoiewio", "easdgsdgasdg" };
+
+            //var res = from str in strings
+            //          select str[0];
+
+            //res = res.Reverse();
+
+            //foreach (var item in res)
+            //{
+            //    Console.Write(item);
+            //}
+            //Console.WriteLine();
+        }
+    }
+}
---
+++ 18.10.2019/18.10.2019/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18._10._2019
+{
+
+    struct Category
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    struct Product
+    {
+        public int id { get; set; }
+        public int id_category { get; set; }
+        public int year { get; set; }
+        public int price { get; set; }
+        public string country { get; set; }
+        public string name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {name}\nPrice: {price}\nCountry: {country}\nYear: {year}\n";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Category[] categories = new Category[]
+            {
+                new Category(){ id = 1, name = "TV"},
+                new Category(){ id = 2, name = "Phone"},
+                new Category(){ id = 3, name = "PC"},
+            };
+
+            Product[] products = new Product[]
+            {
+                new Product(){ id = 1, id_category = 2, year = 2019, price = 12999, country = "China", name = "Posa Galaxy S8"},
+                new Product(){ id = 2, id_category = 2, year = 2018, price = 7699, country = "China", name = "Kile ks90"},
+                new Product(){ id = 3, id_category = 3, year = 2017, price = 45000, country = "UK", name = "Fusoa p190"},
+                new Product(){ id = 4, id_category = 2, year = 2019, price = 16799, country = "USA", name = "Ioa 0091"},
+                new Product(){ id = 5, id_category = 1, year = 2018, price = 9000, country = "Ukraine", name = "Popos o89"},
+            };
+
+
+            /*
+             Завдання 1:
+             Вибрати всі товари, які виготовлені в теперішньому році. 
+             Відсортувати їх за принципом: від найдорожчого до найдешевшого.
+            */
+
+            //var res = from item in products
+            //          where item.year == DateTime.Now.Year
+            //          orderby item.price descending
+            //          select item;
+
+            //foreach (var item in res)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            /*
+            Завдання 2:
+            Знайти кількість продуктів, що виготовлені у вибраній країні.
+            */
+
+            //var res = from item in products
+            //          where item.country == "China"
+            //          select item;
+
+            //Console.WriteLine($"Count: {res.Count()}\n");
+
+
+            /*
+            Завдання 3:
+            Знайти найдорожчий та найдешевший продукт вибраної категорії.
+            */
+
+            //var res = from item in products
+            //          where item.id_category == 2
+            //          orderby item.price ascending
+            //          select item;
+
+            //Console.WriteLine($"Big price:");
+            //Console.WriteLine(res.Last());
+
+            //Console.WriteLine($"Min price:");
+            //Console.WriteLine(res.First());
+
+
+            /*
+            Завдання 4:
+            Показати назви категорій, продукти яких не виготовляються в Україні.
+             */
+
+            //var res = from cat in categories
+            //          join item in products on cat.id equals item.id_category
+            //          where item.country != "Ukraine"
+            //          select cat.name;
+
+            //foreach (var item in res)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+        }
+    }
+}
